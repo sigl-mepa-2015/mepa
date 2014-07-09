47e2db36775c9758abfcd383b0269115ecba33ac
Etienne Coutaud <ecoutaud@advanced-schema.com>
2014-07-10 01:34:18 +0200

    fixed bug, create clean tournament model

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Pool.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Pool.java
@@ -13,57 +13,16 @@ import javax.validation.constraints.NotNull;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-//Cette ligne commentee perme de ne pas generer le schema. L'equipe concerne doit generer le model correspondant avec les relations qui vont bien
-//@Entity
+@Entity
+@Table(name="Pool")
 @NamedQueries({
         @NamedQuery(name = "Pool.findById", query = "FROM Pool o WHERE o.id=:id"),
         @NamedQuery(name = "Pool.findAll", query = "FROM Pool o") })
 public class Pool {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    private Long id;
-    private Set<Match> matchs;
-    
-    private Tournament tournament;
- 
-    private String name;
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    //@OneToMany(mappedBy="pool")
-    //public Set<Match> getMatchs() { return matchs; }
-
-   // @ManyToOne()
-    @JoinColumn(name="TOURNAMENT_ID", nullable=false)
-    public Tournament getTournament() { return tournament; }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-    }
-
-	public Set<Match> getMatchs() {
-		return matchs;
-	}
-
-	public void setMatchs(Set<Match> matchs) {
-		this.matchs = matchs;
-	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id", nullable=false)
+	private Long id;
 
 }
