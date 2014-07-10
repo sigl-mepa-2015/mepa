a1f76e9508db644eb414ea55111dcacf84cac593
Etienne Coutaud <ecoutaud@advanced-schema.com>
2014-07-10 11:42:17 +0200

    add jointure between tournament/pool/game need to test

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Game.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Game.java
@@ -9,6 +9,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -17,7 +19,8 @@ import javax.persistence.Table;
 @Table(name="Game")
 @NamedQueries({
         @NamedQuery(name = "Game.findById", query = "FROM Game o WHERE o.id=:id"),
-        @NamedQuery(name = "Game.findAll", query = "FROM Game o") })
+        @NamedQuery(name = "Game.findAll", query = "FROM Game o") ,
+        @NamedQuery(name = "Game.findAllByPoolId", query = "SELECT g FROM Game g LEFT JOIN g.pool p ON p.id = :poolId")})
 public class Game implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -48,4 +51,120 @@ public class Game implements Serializable{
 	@Enumerated(EnumType.STRING)
 	@Column(name="status", nullable=false)
 	private GameStatus status;
+	
+	@ManyToOne(targetEntity=Pool.class)
+	@JoinColumn(name="POOL_ID")
+	private Pool pool;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the team1
+	 */
+	public Team getTeam1() {
+		return team1;
+	}
+
+	/**
+	 * @param team1 the team1 to set
+	 */
+	public void setTeam1(Team team1) {
+		this.team1 = team1;
+	}
+
+	/**
+	 * @return the team2
+	 */
+	public Team getTeam2() {
+		return team2;
+	}
+
+	/**
+	 * @param team2 the team2 to set
+	 */
+	public void setTeam2(Team team2) {
+		this.team2 = team2;
+	}
+
+	/**
+	 * @return the resultTeam1
+	 */
+	public int getResultTeam1() {
+		return resultTeam1;
+	}
+
+	/**
+	 * @param resultTeam1 the resultTeam1 to set
+	 */
+	public void setResultTeam1(int resultTeam1) {
+		this.resultTeam1 = resultTeam1;
+	}
+
+	/**
+	 * @return the resultTeam2
+	 */
+	public int getResultTeam2() {
+		return resultTeam2;
+	}
+
+	/**
+	 * @param resultTeam2 the resultTeam2 to set
+	 */
+	public void setResultTeam2(int resultTeam2) {
+		this.resultTeam2 = resultTeam2;
+	}
+
+	/**
+	 * @return the duration
+	 */
+	public int getDuration() {
+		return duration;
+	}
+
+	/**
+	 * @param duration the duration to set
+	 */
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public GameStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(GameStatus status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the pool
+	 */
+	public Pool getPool() {
+		return pool;
+	}
+
+	/**
+	 * @param pool the pool to set
+	 */
+	public void setPool(Pool pool) {
+		this.pool = pool;
+	}
 }
--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Pool.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Pool.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -17,7 +19,8 @@ import javax.persistence.Table;
 @Table(name="Pool")
 @NamedQueries({
         @NamedQuery(name = "Pool.findById", query = "FROM Pool o WHERE o.id=:id"),
-        @NamedQuery(name = "Pool.findAll", query = "FROM Pool o") })
+        @NamedQuery(name = "Pool.findAll", query = "FROM Pool o"),
+        @NamedQuery(name = "Pool.findAllByTournamentId", query ="SELECT p FROM Pool p LEFT JOIN p.tournament t ON t.id = :tournamentId")})
 public class Pool implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -36,6 +39,10 @@ public class Pool implements Serializable{
 	@OneToMany
 	private Set<Team> teams;
 
+	@ManyToOne(targetEntity=Tournament.class)
+	@JoinColumn(name="TOURNAMENT_ID")
+	private Tournament tournament;
+	
 	/**
 	 * @return the id
 	 */
@@ -90,6 +97,20 @@ public class Pool implements Serializable{
 	 */
 	public void setTeams(Set<Team> teams) {
 		this.teams = teams;
+	}
+
+	/**
+	 * @return the tournament
+	 */
+	public Tournament getTournament() {
+		return tournament;
+	}
+
+	/**
+	 * @param tournament the tournament to set
+	 */
+	public void setTournament(Tournament tournament) {
+		this.tournament = tournament;
 	}
 
 }
