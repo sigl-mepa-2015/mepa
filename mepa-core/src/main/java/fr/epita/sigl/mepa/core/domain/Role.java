1a5d25e047ee20de0aaf68fa0611fb8f812e2ae9
Alex T <alexandre.taupiac@gmail.com>
2014-07-12 01:31:55 +0200

    beta 2.0 role

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Role.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/Role.java
@@ -1,18 +1,58 @@
 package fr.epita.sigl.mepa.core.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Created by Alexandre on 7/12/2014.
  */
 
 @Entity
-@Table(name = "Player")
+@Table(name = "Role")
 @NamedQueries({
-        @NamedQuery(name = "Player.findById", query = "FROM Player o WHERE o.id=:id"),
-        @NamedQuery(name = "Player.findAll", query = "FROM Player o")})
-public class Role {
+        @NamedQuery(name = "Role.findById", query = "FROM Role o WHERE o.id=:id"),
+        @NamedQuery(name = "Role.findAll", query = "FROM Role o")})
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String authority;
+    private Long id;
+
+    @JoinTable(
+            name="USER_ROLE",
+            joinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")})
+    private Set<User> users;
+
+    /**
+     * @return the id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="POOL_ID", nullable=false)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
