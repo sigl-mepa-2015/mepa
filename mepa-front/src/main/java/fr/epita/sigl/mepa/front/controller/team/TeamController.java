ac864102e973d48d519545e093b870d5d99befa4
TEISSEDRE Florian <teissedre.f@gmail.com>
2014-07-11 17:48:51 +0200

    Creation of the edit team form

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/team/TeamController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/team/TeamController.java
@@ -13,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -42,9 +39,14 @@ public class TeamController {
     }
 
     @RequestMapping(value="/edit", method=RequestMethod.GET)
-    public String editTeam(){
+    public ModelAndView editTeam(@RequestParam("teamID") Long teamID){
 
-        return "/team/edit/form";
+        Team team = teamService.getTeamById(teamID);
+        System.out.println(teamID);
+        ModelAndView mv = new ModelAndView("/team/edit/form");
+        mv.addObject("t", team);
+        return mv;
+
     }
     
     
--- mepa-front/src/main/webapp/WEB-INF/views/team/edit/form.jsp
+++ mepa-front/src/main/webapp/WEB-INF/views/team/edit/form.jsp
@@ -1,4 +1,19 @@
 <%@ include file="/WEB-INF/views/includes/common.jsp"%>
 <%@ include file="/WEB-INF/views/team/namespace.jsp"%>
 
-Edition
+<div class="container">
+    <div class="page-header">
+        <h1><spring:message code="team.editTitle" /> : ${team.getName()}</h1>
+    </div>
+    <form:form role="form" action="${addTeamFormActionUrl}" modelAttribute="addTeamFormBean" method="post">
+        <div class="form-group col-md-3">
+            <form:errors path="name" cssStyle="color: #FF0000;" htmlEscape="false"/>
+            <label for="name"><spring:message code="team.form.nameLabel" /></label>
+            <br/>
+            <spring:message code='team.form.namePlaceholder' var="namePlaceholder"/>
+            <form:input id="name" path="name" class="form-control" type="text" maxlength="20" placeholder="${namePlaceholder}"/>
+            <br/>
+            <button type="submit" class="btn btn-primary"><spring:message code="add" /></button>
+        </div>
+    </form:form>
+</div>
