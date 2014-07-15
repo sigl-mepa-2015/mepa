646d664e9d1d83a57979cfbfe825f750a301fcf8
Etienne Coutaud <e.coutaud@gmail.com>
2014-07-15 17:45:11 +0200

    get score

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/JoinedGameTeam.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/JoinedGameTeam.java
@@ -9,7 +9,10 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "JoinedGameTeam")
-@NamedQueries({@NamedQuery(name = "JoinedGameTeam.findAllByGameId", query = "FROM JoinedGameTeam j WHERE j.game.id=:gameId")})
+@NamedQueries({@NamedQuery(name = "JoinedGameTeam.findAllByGameId", query = "FROM JoinedGameTeam j WHERE j.game.id=:gameId"),
+	@NamedQuery(name = "JoinedGameTeam.getScoreGoalByTeamId", 
+		query = "SELECT sum(j.score) FROM JoinedGameTeam j WHERE j.team.id=:teamId")})
+
 public class JoinedGameTeam implements Serializable {
 
     private static final long serialVersionUID = 1L;
