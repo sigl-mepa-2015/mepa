c2fc33c1d6bddfc6d65c0a349a6eeaa952bd11d5
Lembi Jason <jason.lembi@epita.fr>
2014-07-18 17:12:24 +0200

    Réglage bug après message envoyé

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/result/ResultController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/result/ResultController.java
@@ -136,7 +136,7 @@ public class ResultController {
         else
             g.setStatus(Game.GameStatus.DONE);
          
-        this.gs.updateGame(g);
+     
         
         if (request.getParameter("resultEquipe1").compareTo("") != 0) {
             Long joinedGameTeam1 = Long.parseLong(request.getParameter("joinedID1"));
@@ -164,7 +164,7 @@ public class ResultController {
             return "redirect:afficherGame?poolID="+g.getPool().getId();
         }
         
-       
+          this.gs.updateGame(g);
         
          if (request.getParameter("Status").compareTo("En cours") == 0)
         message = "validerLive";
